6f09c39652d2488465a1a047e8f4045315cf07a2
Sara Collins <saracollins0508@gmail.com>
2018-07-22 19:00:05 -0500

    Added code for adding additional names to characters and characters in a universe

--- universe_map/character.py
+++ universe_map/character.py
@@ -1,3 +1,45 @@
 class Character:
 	def __init__(self):
-		self._name = None
+		self._names = []
+		
+	def add_name(self, type, names, name_types):
+		self._names.append(CharacterName(type, names, name_types)
+		
+class CharacterName:
+	def __init__(self, type, names, name_types):
+		# First, assert that names and types are the same length
+		assert len(names) == len(types), "Names and types lists need to be the same length."
+		self._type = general_type(general_type)
+		self._name = name(names, types)
+		
+	@property
+	def type(self):
+		return self._type
+		
+	@type.setter
+	def type(self, value):
+		self._type = value
+		
+	@property
+	def name(self):
+		return self._name
+	
+	@name.setter
+	def name(self, names, name_types):
+		# List being used to preserve indexing order
+		name_list = []
+		for i in range(len(names)):
+			char_dict = {} 
+			char_dict['name'] = names[i]
+			char_dict['type'] = name_types[i]
+			name_list.append(char_dict)
+		return name_list
+	
+	@property
+	def full_name_string(self):
+		return " ".join([self._name[x]['name'] for x in range(len(self._name))])
+		
+		
+	
+	
+	
--- universe_map/universe.py
+++ universe_map/universe.py
@@ -1,6 +1,7 @@
 class Universe:
 	def __init__(self):
 		self._name = None
+		self._characters = []
 		
 	@property
 	def name(self):
@@ -13,3 +14,16 @@ class Universe:
   @name.deleter
   def name(self):
   	del self._name
+  	
+  @property
+  def characters(self):
+  	return self._characters
+  
+  @characters.setter
+  def characters(self, list_value):
+  	self._characters = list_value
+  
+  def add_character(character_object):
+  	self._characters.append(character_object)
+  
+  
